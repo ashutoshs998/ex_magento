8eca319b048d360a041c1f78577deb3db1cd80c4
ashutosh_m <ashutosh_m@excellencetechnologies.in>
2017-10-07 16:01:23 +0530

    res.render solved

--- app.js
+++ app.js
@@ -75,12 +75,13 @@ app.use(function (req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500).json({message: err.message,
+            error: err});
+        // res.render('error', {
+        //     message: err.message,
+        //     error: err
+        // });
     });
 }
 
